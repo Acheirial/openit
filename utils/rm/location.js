5e40ce8e09ade94a004f9477566ded3e12dcceeb
Acheirial <224619126+Acheirial@users.noreply.github.com>
2026-08-31 03:32:17 +0800

    2026.08.31 03:32:17 修正国家解析 DNS

--- utils/rm/location.js
+++ utils/rm/location.js
@@ -14,22 +14,25 @@ function countryOf(ip){
 module.exports={
     countryOf,
     async resolve(name){
-        let domainReg = /[a-zA-Z0-9][-a-zA-Z0-9]{0,62}(.[a-zA-Z0-9][-a-zA-Z0-9]{0,62})+.?/g
-        let ipReg = /((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]\d)|\d)(\.((25[0-5])|(2[0-4]\d)|(1\d\d)|([1-9]\d)|\d)){3}/
-        if(ipReg.test(name)){
-            return name
-        }else if(domainReg.test(name)){
-            try{
-                let address = await resolver.resolve4(name);
-                if(address && address[0]){
-                    return address[0]
-                }
-                return null
-            }catch(e){
-                return null
-            }
+        const host = String(name || '').replace(/^\[/, '').replace(/\]$/, '')
+        const ipv4 = /^(?:(?:25[0-5]|2[0-4]\d|1?\d?\d)\.){3}(?:25[0-5]|2[0-4]\d|1?\d?\d)$/
+        const ipv6 = /^[0-9a-fA-F:]+$/
+        const domain = /^(?=.{1,253}$)(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,}$/
+        if(ipv4.test(host) || (host.includes(':') && ipv6.test(host))){
+            return host
         }
-        return null
+        if(!domain.test(host)){
+            return null
+        }
+        try{
+            let address = await resolver.resolve4(host);
+            if(address && address[0]){
+                return address[0]
+            }
+            return null
+        }catch(e){
+            return null
+        }
     },
     async get(name){
         let ip = await this.resolve(name)
